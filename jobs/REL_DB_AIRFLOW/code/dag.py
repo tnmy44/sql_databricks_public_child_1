635ebed926966ffa487586b35bb61014c92c97e8
abhisheks@prophecy.io <abhisheks@prophecy.io>
2023-05-17 17:22:53 +0000

    🔁 Modified     - Job        - REL_DB_Databricks        - REL_DB_AIRFLOW

--- jobs/REL_DB_AIRFLOW/code/dag.py
+++ jobs/REL_DB_AIRFLOW/code/dag.py
@@ -3,23 +3,20 @@ import sys
 import pendulum
 from datetime import timedelta
 import airflow
-from airflow.decorators import dag
+from airflow import DAG
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 from tasks import DBT_0, Script_1, Script_2
 PROPHECY_RELEASE_TAG = "__PROJECT_ID_PLACEHOLDER__/__PROJECT_RELEASE_VERSION_PLACEHOLDER__"
 
-@dag(
-     dag_id = "uitesting_shared_team_SQL_DatabricksSharedBasic_REL_DB_AIRFLOW", 
-     schedule_interval = "0 0 17 * *", 
-     default_args = {"owner" : "Prophecy", "ignore_first_depends_on_past" : True, "do_xcom_push" : True}, 
-     start_date = airflow.utils.dates.days_ago(0), 
-     catchup = True
-)
-def dag():
+with DAG(
+    dag_id = "uitesting_shared_team_SQL_DatabricksSharedBasic_REL_DB_AIRFLOW", 
+    schedule_interval = "0 0 17 * *", 
+    default_args = {"owner" : "Prophecy", "ignore_first_depends_on_past" : True, "do_xcom_push" : True}, 
+    start_date = pendulum.today('UTC'), 
+    catchup = True
+) as dag:
     DBT_0_op = DBT_0()
     Script_1_op = Script_1()
     Script_2_op = Script_2()
     DBT_0_op >> Script_1_op
     Script_1_op >> Script_2_op
-
-dag()
