21e5109d14674f36043efc399066c0e81dba3c49
abhisheks@prophecy.io <abhisheks@prophecy.io>
2023-07-06 15:56:47 +0000

    🔁 Modified     - Job        - REL_DB_PMAirlow

--- jobs/REL_DB_PMAirlow/code/r7jkhvrxm_nq6nicb3n5uq_/tasks/Slack_1.py
+++ jobs/REL_DB_PMAirlow/code/r7jkhvrxm_nq6nicb3n5uq_/tasks/Slack_1.py
@@ -4,7 +4,7 @@ def Slack_1():
 
     return SlackAPIPostOperator(
         task_id = "Slack_1",
-        text = "Test Slack message",
+        text = "Test Slack message DB Airflow PM",
         channel = "abhyslackpub",
         slack_conn_id = "7k_Cby3g6vOgeKrdy93nb",
         **settings
