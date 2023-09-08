0937c7a073439c37a752d433daf4f910eb3cb4a5
abhisheks@prophecy.io <abhisheks@prophecy.io>
2023-09-08 12:08:41 +0000

    🔁 Modified     - Job        - REL_DB_MWAA

--- jobs/REL_DB_MWAA/code/uitesting_shared_team_sql_childdatabricksshared_rel_db_mwaa/tasks/Script_2.py
+++ jobs/REL_DB_MWAA/code/uitesting_shared_team_sql_childdatabricksshared_rel_db_mwaa/tasks/Script_2.py
@@ -1,6 +1,5 @@
 def Script_2():
-    settings = {}
     from datetime import timedelta
     from airflow.operators.bash import BashOperator
 
-    return BashOperator(task_id = "Script_2", bash_command = "date", **settings)
+    return BashOperator(task_id = "Script_2", bash_command = "date", )
