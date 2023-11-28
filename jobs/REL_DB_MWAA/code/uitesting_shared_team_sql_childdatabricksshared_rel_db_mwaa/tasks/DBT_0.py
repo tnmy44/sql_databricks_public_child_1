444fc6325dd598f27a236ff4b03c1c6d55f8a439
abhisheks@prophecy.io <abhisheks@prophecy.io>
2023-11-28 10:02:47 +0000

    🔁 Modified     - Job        - REL_DB_MWAA

--- jobs/REL_DB_MWAA/code/uitesting_shared_team_sql_childdatabricksshared_rel_db_mwaa/tasks/DBT_0.py
+++ jobs/REL_DB_MWAA/code/uitesting_shared_team_sql_childdatabricksshared_rel_db_mwaa/tasks/DBT_0.py
@@ -5,12 +5,7 @@ def DBT_0():
     return BashOperator(
         task_id = "DBT_0",
         bash_command = "set -euxo pipefail; tmpDir=`mktemp -d`; git clone https://github.com/abhisheks-prophecy/sql_databricks_public_child_1 --branch dev_staging --single-branch $tmpDir; cd $tmpDir/; dbt deps --profile run_profile; dbt seed --profile run_profile --threads=2; dbt run --profile run_profile --threads=2; ",
-        env = {
-          "DBT_PROFILES_DIR": "/usr/local/airflow/dags", 
-          "DBT_FAIL_FAST": "true", 
-          "DBT_PRINT": "false", 
-          "DBT_FULL_REFRESH": "true"
-        },
+        env = {"DBT_PROFILES_DIR" : "/usr/local/airflow/dags", "DBT_PRINT" : "false", "DBT_FULL_REFRESH" : "true"},
         append_env = True,
         retry_exponential_backoff = True, 
         retries = 1
