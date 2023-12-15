635af87f3edc092c5ca9eb8ce7ec2b807eb3bfa1
abhisheks@prophecy.io <abhisheks@prophecy.io>
2023-12-15 09:40:30 +0000

    🔁 Modified     - Job        - REL_DB_AIRFLOW

--- jobs/REL_DB_AIRFLOW/code/uitesting_shared_team_sql_databrickssharedbasic_rel_db_airflow/tasks/DBT_0.py
+++ jobs/REL_DB_AIRFLOW/code/uitesting_shared_team_sql_databrickssharedbasic_rel_db_airflow/tasks/DBT_0.py
@@ -5,13 +5,8 @@ def DBT_0():
     return BashOperator(
         task_id = "DBT_0",
         bash_command = "set -euxo pipefail; tmpDir=`mktemp -d`; git clone https://github.com/abhisheks-prophecy/sql_databricks_public_child_1 --branch  {{ params.branch }} --single-branch $tmpDir; cd $tmpDir/; dbt deps --profile run_profile; dbt seed --profile run_profile --threads=1; dbt run --profile run_profile --threads=1; ",
-        env = {
-          "DBT_PROFILES_DIR": "/home/airflow/gcs/data", 
-          "DBT_FAIL_FAST": "true", 
-          "DBT_PRINT": "false", 
-          "DBT_FULL_REFRESH": "true"
-        },
+        env = {"DBT_PROFILES_DIR" : "/home/airflow/gcs/data", "DBT_PRINT" : "false", "DBT_FULL_REFRESH" : "true"},
         append_env = True,
         retry_exponential_backoff = True, 
-        retries = 1
+        retries = 0
     )
