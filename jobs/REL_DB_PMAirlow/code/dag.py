8ae9617645c854dd4368d310cc39fd0d6a577026
abhisheks@prophecy.io <abhisheks@prophecy.io>
2024-05-14 06:56:25 +0000

    🔁 Modified     - Job        - Staging_REL_DB_AIRFLOW        - Staging_REL_DB_PMAirlow

--- jobs/REL_DB_PMAirlow/code/dag.py
+++ jobs/REL_DB_PMAirlow/code/dag.py
@@ -12,7 +12,7 @@ PROPHECY_RELEASE_TAG = "__PROJECT_ID_PLACEHOLDER__/__PROJECT_RELEASE_VERSION_PLA
 
 with DAG(
     dag_id = "bkENgaNaF_ns4_zWtM_Cgw_", 
-    schedule_interval = "0 0 17 * *", 
+    schedule_interval = "0 0 * * 1", 
     default_args = {
       "owner": "Prophecy", 
       "retries": 1, 
