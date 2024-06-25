2161b32fcb7c883a5024251e334c8c6a4f5bedf5
abhisheks@prophecy.io <abhisheks@prophecy.io>
2024-06-25 11:01:20 +0000

    Updates REL_DB_PMAirlow job to remove DBT profile secret and correct boolean value format

--- jobs/REL_DB_PMAirlow/code/bkenganaf_ns4_zwtm_cgw_/tasks/DBT_0_1.py
+++ jobs/REL_DB_PMAirlow/code/bkenganaf_ns4_zwtm_cgw_/tasks/DBT_0_1.py
@@ -17,13 +17,13 @@ def DBT_0_1():
           "DBT_PRINT": "false", 
           "DBT_LOG_PATH": "logs.txt", 
           "DBT_FULL_REFRESH": "true", 
-          "DBT_PROFILE_SECRET": "61Yi8DU2q7fwUQEPT6O0S", 
+          "DBT_PROFILE_SECRET": "", 
           "GIT_TOKEN_SECRET": "lfJ5vC2nkIrYJFHPCPJU9Q_", 
           "GIT_ENTITY": "tag", 
           "GIT_ENTITY_VALUE": "__PROJECT_FULL_RELEASE_TAG_PLACEHOLDER__", 
           "GIT_SSH_URL": "https://github.com/abhisheks-prophecy/sql_databricks_public_child_1", 
           "GIT_SUB_PATH": "", 
-          "IS_ADHOC_RUN_FROM_SAME_PROJECT": False or False
+          "IS_ADHOC_RUN_FROM_SAME_PROJECT": "false"
         },
         append_env = True,
         retry_exponential_backoff = True, 
