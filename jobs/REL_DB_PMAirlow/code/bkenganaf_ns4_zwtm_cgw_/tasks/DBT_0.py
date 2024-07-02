48f8cf745e3980802a6d66b6ffe1e097e5a51f68
abhisheks@prophecy.io <abhisheks@prophecy.io>
2024-07-02 09:46:27 +0000

    Updates REL_DB_AIRFLOW and REL_DB_PMAirlow job configurations and dependencies

--- jobs/REL_DB_PMAirlow/code/bkenganaf_ns4_zwtm_cgw_/tasks/DBT_0.py
+++ jobs/REL_DB_PMAirlow/code/bkenganaf_ns4_zwtm_cgw_/tasks/DBT_0.py
@@ -2,6 +2,7 @@ from bkenganaf_ns4_zwtm_cgw_.utils import *
 
 def DBT_0():
     from airflow.operators.bash import BashOperator
+    from datetime import timedelta
     import os
     import zipfile
     import tempfile
@@ -16,13 +17,13 @@ def DBT_0():
           "DBT_FAIL_FAST": "true", 
           "DBT_PRINT": "false", 
           "DBT_FULL_REFRESH": "true", 
-          "DBT_PROFILE_SECRET": "61Yi8DU2q7fwUQEPT6O0S", 
+          "DBT_PROFILE_SECRET": "NBGW5z7eP5DyFVJyclt-j", 
           "GIT_TOKEN_SECRET": "lfJ5vC2nkIrYJFHPCPJU9Q_", 
           "GIT_ENTITY": "tag", 
           "GIT_ENTITY_VALUE": "__PROJECT_FULL_RELEASE_TAG_PLACEHOLDER__", 
           "GIT_SSH_URL": "https://github.com/abhisheks-prophecy/sql_databricks_public_child_1", 
           "GIT_SUB_PATH": "", 
-          "IS_ADHOC_RUN_FROM_SAME_PROJECT": False or False
+          "IS_ADHOC_RUN_FROM_SAME_PROJECT": "false"
         },
         append_env = True,
         retry_exponential_backoff = True, 
