d19139847ab618ed423359cc711a6d6fe9ee33f2
abhisheks@prophecy.io <abhisheks@prophecy.io>
2024-07-30 14:47:37 +0000

    Updates uitesting_specifycolumns model with new columns and variables

--- models/write_modes_others/uitesting_specifycolumns.sql
+++ models/write_modes_others/uitesting_specifycolumns.sql
@@ -2,14 +2,16 @@
   config({    
     "materialized": "incremental",
     "incremental_predicates": [
-      'DBT_INTERNAL_SOURCE.c_tinyint > -10
+      "DBT_INTERNAL_SOURCE.c_tinyint > -10
       AND CASE
             WHEN DBT_INTERNAL_DEST.c_struct.city IS NOT NULL
               THEN true
             WHEN DBT_INTERNAL_SOURCE.c_struct.pin IS NOT NULL
               THEN false
+            WHEN array_contains(DBT_INTERNAL_SOURCE.c_array, DBT_INTERNAL_DEST.c_string) = true
+              THEN false
             ELSE false
-          END'
+          END"
     ],
     "incremental_strategy": 'merge',
     "merge_update_columns": ['c_tinyint', 'c_smallint', 'c_int'],
@@ -17,6 +19,10 @@
     "unique_key": ['c_id']
   })
 }}
+
+{% set v_model_int_real = 22 %}
+{% set v_model_string_real = "modelstr" %}
+
 
 WITH all_type_non_partitioned_1 AS (
 
@@ -28,7 +34,7 @@ WITH all_type_non_partitioned_1 AS (
 
 all_type_non_partitioned_columns AS (
 
-  {#Extracts all non-partitioned columns from the 'all_type_non_partitioned' table.#}
+  {#Retrieves a comprehensive dataset of various data types for analysis.#}
   SELECT 
     c_tinyint AS c_tinyint,
     c_smallint AS c_smallint,
@@ -40,7 +46,9 @@ all_type_non_partitioned_columns AS (
     c_boolean AS c_boolean,
     c_array AS c_array,
     c_struct AS c_struct,
-    monotonically_increasing_id() AS c_id
+    monotonically_increasing_id() AS c_id,
+    CURRENT_DATE AS c_date,
+    CURRENT_TIMESTAMP() AS c_timestamp
   
   FROM all_type_non_partitioned_1 AS in0
 
